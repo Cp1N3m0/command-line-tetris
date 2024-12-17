8f5cdcd8f88425a8d6d5989edd8756692ca81423
Cp1N3m0 <122443676+Cp1N3m0@users.noreply.github.com>
2024-12-17 13:39:02 +0100

    Update game.py
    Change all Block colours to green

--- game.py
+++ game.py
@@ -296,13 +296,13 @@ def init_colors():
 	"""
 	Initializes curses colors for all pieces
 	"""
-	curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_BLACK)
-	curses.init_pair(2, curses.COLOR_CYAN, curses.COLOR_BLACK)
+	curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
 	curses.init_pair(3, curses.COLOR_GREEN, curses.COLOR_BLACK)
-	curses.init_pair(4, curses.COLOR_YELLOW, curses.COLOR_BLACK)
-	curses.init_pair(5, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
-	curses.init_pair(6, curses.COLOR_RED, curses.COLOR_BLACK)
-	curses.init_pair(7, curses.COLOR_WHITE, curses.COLOR_BLACK)
+	curses.init_pair(4, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(5, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(6, curses.COLOR_GREEN, curses.COLOR_BLACK)
+	curses.init_pair(7, curses.COLOR_GREEN, curses.COLOR_BLACK)
 
 
 def re_draw_piece(window, piece: AbstractPiece):
